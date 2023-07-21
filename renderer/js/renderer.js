c0b7e943e6a02c7ef7bd9ba6a409b3da3920386b
Ali Ismail <aismail102@gmail.com>
2023-07-21 14:45:35 -0400

    Added a family feud background; strting to work backwards to simplify the page

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('#img-form');
+const img  = document.querySelector('#img');
+const outputPath  = document.querySelector('#output-path');
+const filename    = document.querySelector('#filename');
+const heightInput = document.querySelector('#height');
+const widthInput  = document.querySelector('#width');
 
+function loadImage(e)
+{
+    const file = e.target.files[0];
+}
+
+// Make sure file is image
+
+img.addEventListener('change', loadImage);
