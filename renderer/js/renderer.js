b849072d0dc8680fd6ae47207602331c062b5932
Ali Ismail <aismail102@gmail.com>
2023-07-22 00:42:04 -0400

    Added round 1 intro and audio

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,12 +1,32 @@
-const startBtn = document.getElementById("startBtn");
+const startBtn        = document.getElementById("startBtn");
+const round1Img       = document.getElementById("round1Img");
+const familyFeudAudio = document.getElementById("familyFeudAudio");
 
 // Event Listeners
 
 // When user presses start button, start the first round
-startBtn.addEventListener("click", startRound, 1);
+startBtn.addEventListener("click", () => {introduceRound(1)});
+
+
+// Functions
 
 // Display start round
- function startRound(round)
+function introduceRound(round)
 {
-    startBtn.style.display = 'none';
+    if(round === 1)
+    {
+        // Add a listener to detect that the animation ended
+        round1Img.addEventListener('animationend', animationEndCallback);
+        familyFeudAudio.play();
+        startBtn.style.display              = 'none';
+        document.body.style.backgroundImage = 'none';
+        round1Img.style.display             = 'inline';
+    }
+}
+
+animationEndCallback = (e) => 
+{
+    round1Img.removeEventListener('animationend', animationEndCallback);
+    familyFeudAudio.pause();
+    round1Img.style.display = 'none';
 }
