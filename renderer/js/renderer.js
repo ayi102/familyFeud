7865bc130f43b6fc4b37bfe6233d6aa4602b373f
Ali Ismail <aismail102@gmail.com>
2023-07-24 20:50:53 -0400

    Parse in guess inputs; Initail code for json parsing

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -3,12 +3,14 @@ const round1Img       = document.getElementById("round1Img");
 const familyFeudAudio = document.getElementById("familyFeudAudio");
 const gameBoard       = document.getElementById("gameBoard");
 const guesses         = document.getElementById("guesses");
+const guess           = document.getElementById("myGuess");
 const questions       = document.getElementById("questions");
 
 // Event Listeners
 
 // When user presses start button, start the first round
 startBtn.addEventListener("click", () => {introduceRound(1)});
+guesses.addEventListener("keypress", (e) => {parseInput(e)});
 
 
 // Functions
@@ -23,12 +25,12 @@ function introduceRound(round)
         startBtn.style.display              = 'none';
         document.body.style.backgroundImage = 'none';
         round1Img.style.display             = 'inline';
-        familyFeudAudio.addEventListener('ended', () => {playRound(1)});
+        familyFeudAudio.addEventListener('ended', () => {displayRound(1)});
     }
 }
 
 // Display the round play
-function playRound(round)
+function displayRound(round)
 {
     if(round === 1)
     {
@@ -39,4 +41,27 @@ function playRound(round)
         guesses.style.display               = 'block';
         questions.style.display             = 'block';
     }
+}
+
+// Parse the entered key
+function parseInput(e)
+{
+    if(e.key === 'Enter')
+    {
+        // Determine if the guess
+        guess.value = "";
+    }
+}
+
+// Wrapper for requesting JSON
+async function getJson() {
+    const json = await requestJson('./metadata/qAndA.json');
+    return json
+}
+
+// request json data
+async function requestJson(url) {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
 }
